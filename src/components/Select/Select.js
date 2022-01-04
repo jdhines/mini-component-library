9894379b4e0e1722e2bedc04702fdc7ce49921e2
Jess Hines <jess.hines@fmr.com>
2022-01-04 16:13:04 -0700

    Second attempt at Select component using some ideas from the video

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -11,54 +11,48 @@ const Select = ({label, value, onChange, children}) => {
   return (
     <Wrapper value={value} displayValue={displayedValue} onChange={onChange}>
       <SelectWrapper>{children}</SelectWrapper>
-      <IconWrapper>
-        <Icon id="chevron-down" size="12" strokeWidth="3" />
-      </IconWrapper>
+      <PresentationalBits>
+        {displayedValue}
+        <Icon id="chevron-down" size="16" strokeWidth="3" />
+      </PresentationalBits>
     </Wrapper>
   );
 };
 
-const IconWrapper = styled.div`
-  grid-area: select;
-  justify-self: end;
-  padding-right: 12px;
-`;
-
 // helped out by:
 // https://moderncss.dev/custom-select-styles-with-pure-css/
 // Only issue is that clicking the chevron itself doesn't activate the dropdown
+const Wrapper = styled.div`
+  color: ${COLORS.gray700};
+  border-radius: 8px;
+  background: ${COLORS.transparentGray15};
+  width: max-content;
+  position: relative;
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const PresentationalBits = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-gap: 1rem;
+  align-items: center;
+  padding: 12px;
+`;
+
 const SelectWrapper = styled.select`
   appearance: none;
   -webkit-appearance: none;
   background: transparent;
   border: none;
   color: transparent;
-  grid-area: select;
+  position: absolute;
+  top: 0;
+  left: 0;
   height: 100%;
   width: 100%;
-  padding: 12px;
-`;
-
-const Wrapper = styled.div`
-  color: ${COLORS.gray700};
-  display: grid;
-  grid-template-areas: 'select';
-  align-items: center;
-  border-radius: 8px;
-  background: ${COLORS.transparentGray15};
-  min-width: 10rem;
-  width: ${(props) => 0.8 * props.displayValue.length + 'em'};
-
-  &:before {
-    content: ${(props) => '"' + props.displayValue + '"'};
-    grid-area: select;
-    padding-left: 12px;
-    color: inherit;
-  }
-
-  &:hover {
-    color: ${COLORS.black};
-  }
 `;
 
 export default Select;
